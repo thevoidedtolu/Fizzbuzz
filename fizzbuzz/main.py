4c1ca6316e7a31d54e35b9a323e5a1653857520f
Tolu Joseph <114223119+thevoidedtolu@users.noreply.github.com>
2022-11-10 13:36:32 +0100

    Add files via upload

---
+++ fizzbuzz/main.py
@@ -0,0 +1,13 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    if number % 3 == 0 and number % 5 != 0:
+        return "Fizz"
+    if number % 3 != 0 and number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
+
+for number in range(1, 101):
+    print(fizzbuzz(number))
